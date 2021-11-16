d3d0b446df7bd79ffd9b72046c119091d97d18e1
osmar.compasso <osmar.rehbein@compasso.com.br>
2021-11-16 09:32:26 -0300

    add unit test

--- target/classes/import.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO invoice_table (id, name, total_items_amount, total_shipping_amount, total_amount) VALUES (1000, 'pessoa1', 100, 10, 110);
-
-INSERT INTO invoice_item (id, product_description, item_amount, invoice_id) VALUES (1000, 'feijao', 50, 1000);
-INSERT INTO invoice_item (id, product_description, item_amount, invoice_id) VALUES (1001, 'arroz', 50, 1000);
